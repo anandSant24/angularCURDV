ac1cc97c9cabd7f43cbd70375d03136dbaaaf028
AnandKumar24 <anandk.kumar506@gmail.com>
2020-01-12 14:06:04 -0800

    notice that both employee and employeeId are getting logged

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { Input, Component, OnInit } from '@angular/core';
+import { Input, Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { Employee } from "../models/employee.models";
 
 @Component({
@@ -7,13 +7,32 @@ import { Employee } from "../models/employee.models";
   styleUrls: ['./display-employee.component.css']
 })
 export class DisplayEmployeeComponent implements OnInit {
-
+  _employee:Employee
+  @Input() employeeId:Number;
+  
   @Input()
-  employee: Employee;
+  set employee(val: Employee){
+    // console.log('previous value = ',this._employee? this._employee.name: null);
+    this._employee = val;
+    // console.log('Current Val', this._employee);
+  }
+  get employee(): Employee{
+    return this._employee;
+  }
+  // employee: Employee;
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  
+  ngOnChanges(changes: SimpleChanges){
+    for(const propKey of Object.keys(changes)){
+      console.log(propKey)
+      // let change = changes[propKey];
+      // let from = JSON.stringify(change.previousValue);
+      // let to = JSON.stringify(change.currentValue);
+      // console.log("change from "+from +"to "+ to);
+    }
+  }
 }
--- src/app/employees/listEmployees.component.ts
+++ src/app/employees/listEmployees.component.ts
@@ -1,22 +1,35 @@
 import { Component, OnInit } from "@angular/core";
 import { Employee } from "../models/employee.models";
 import { EmployeeService } from './employee.service';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
 
 @Component({
   templateUrl: "listEmployees.component.html",
   styleUrls: ["./listEmployees.component.css"]
 })
 export class ListEmployeesComponent implements OnInit {
-  
+  indexEmp:number = 1;
+  employees: Employee[];
+   
   constructor(private empSvc: EmployeeService){
   }
+  employeeToDisplay:Employee;
+  
   ngOnInit(){
     this.employees = this.empSvc.getEmployees();
+    this.employeeToDisplay = this.employees[0];
   }
-  employees: Employee[];
-  
+
   addNewEmployee(newEmployee:Employee):void {
     this.employees.push(newEmployee);
+  }
+
+  nextEmployee(): void{
+    if(this.indexEmp <=2){
+      this.employeeToDisplay = this.employees[this.indexEmp];
+      this.indexEmp++;
+    }else{
+      this.employeeToDisplay = this.employees[0];
+    }
   }
 }
